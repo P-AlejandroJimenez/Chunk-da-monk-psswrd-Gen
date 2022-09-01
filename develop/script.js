424936754543f82660b7c315073a6c0257a5da59
P-AlejandroJimenez <110042398+P-AlejandroJimenez@users.noreply.github.com>
2022-09-01 13:40:25 -0600

    A function and localized variables were added in order to increase functionality for user password customization.

--- develop/script.js
+++ develop/script.js
@@ -1,10 +1,39 @@
 // Assignment Code
+// A variable was added to account for password length.
+var passwordLength = (18);
 var generateBtn = document.querySelector("#generate");
+
+// A function was added to initiate the following branch of code based on the user's input.
+var generatePassword = function() {
+  var userChoice = window.prompt ("Select Password Length:");
+// Localized variables were added within the function to specify the type of characters used in the password generated.
+  if (userChoice < 8) {
+    window.alert("Your password is too short!")
+    return;
+
+  } else if (userChoice > 128) {
+    window.alert("Your password is too Long!")
+    return;
+  }
+// If and else if statements were added with corresponding window.alerts in order to confirm user choices. 
+  var userChoice = window.confirm ("Include numbers?");
+  
+  if (userChoice === true) {
+    window.alert("Numbers included in password!")
+
+  } else if (userChoice === False) {
+    window.alert("Numbers have been excluded from password!")
+  }
+
+  var userChoice = window.confirm ("Include special characters?");
+  var userChoice = window.confirm ("Include uppercase characters?");
+  var userChoice = window.confirm ("Include lowercase characters?");
+};
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password"); 
 
   passwordText.value = password;
 
